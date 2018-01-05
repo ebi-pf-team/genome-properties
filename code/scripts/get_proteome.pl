8c953be67640ba482f711426f256cbb443320943
rdfinn <robert.d.finn@gmail.com>
2018-01-05 12:42:47 +0000

    Code for performing shattering and gathering of I5 searches.
    This code now split files, submits, waits and then joins results back together again.

--- code/scripts/get_proteome.pl
+++ code/scripts/get_proteome.pl
@@ -18,8 +18,6 @@ GetOptions( "out=s"     => \$outDir,
             "help"      => \$help    ) or die "Unknown option\n";
 
 #This means Complete proteome
-my $keyword =  'keyword:181';
-
 if(!$outDir){
   die "Please specify an output directory, see --help for more details\n";
 }
@@ -52,12 +50,12 @@ if(!-e $outDir."/taxonomy/proteomes"){
   mkdir($outDir."/taxonomy/proteomes") or die "Could not make directory $outDir/taxonomy/proteomes";
 }
 chdir($outDir."/taxonomy/proteomes") or die "Could not change to directory $outDir/taxonomy/proteomes";
-
+my $cwd = $outDir."/taxonomy/proteomes";
 # For each taxon, mirror its proteome set in FASTA format.
 for my $taxon (keys %$taxids) {
-   
+  my $proteome = $taxids->{$taxon}->{UPid}; 
   my $file = $taxon . '.fasta';
-  my $query_taxon = "http://www.uniprot.org/uniprot/?query=organism:$taxon+$keyword&format=fasta&include=yes";
+  my $query_taxon = "http://www.uniprot.org/uniprot/?query=proteome:$proteome&format=fasta&include=yes";
   my $response_taxon = $agent->mirror($query_taxon, $file);
 
   if ($response_taxon->is_success) {
@@ -74,20 +72,107 @@ for my $taxon (keys %$taxids) {
 
   #Now check that the file has a size.
   if(!-s $file) {
-    warn "The taxid, $taxon, has no file size\n";
-  }else{
-    my $chunk = 1;
-    my $noSeqs;
-    open(F, "<", $file) or die "Could not open $file for reading\n";
-    while(<F>){
-      $noSeqs++ if(/^>/)
+    die "The taxid, $taxon, has no file size\n";
+  }
+}  
+ 
+#Now loop oevr and chunk the files read for I5.
+for my $taxon (keys %$taxids){
+  print STDERR "Chunking $taxon\n";  
+  my $file = $taxon . '.fasta';
+  my $chunk = 1;
+  my $noSeqs = 1;
+  my $totalSeqs;
+  open(F, "<", $file) or die "Could not open $file for reading\n";
+  open(C, ">", $file.".$chunk") or die "Could not open $file for reading\n";
+  while(<F>){
+    if(/^>/){
+      $noSeqs++;
+      $totalSeqs++;
     }
-    close(F);
-    print "The proteome $taxon contained $noSeqs sequences\n";
+    if($noSeqs > 5000){
+      close(C);
+      $chunk++;
+      open(C, ">", $file.".$chunk") or die "Could not open $file for reading\n";
+      $noSeqs = 1; 
+    }
+    print C $_;
+  }
+  close(F);
+  close(C);
+  $taxids->{$taxon}->{chunk} = $chunk;
+  print "The file $file contains $totalSeqs\n";
+}
+
+
+mkdir("$cwd/i5_analysis") if(!-d "i5_analysis");
+mkdir("$cwd/i5_logs") if(!-d "i5_logs");
+
+
+for my $taxon (keys %$taxids){
+  print "Preparing to submit $taxon\n";
+  my $chunk = $taxids->{$taxon}->{chunk};
+
+  while($chunk){
+    my $file = $taxon . '.fasta.'.$chunk;
+    #bsub -q production-rh7 -n 8 -M 8000 -J i5onhps /hps/nobackup/production/interpro/sw/interproscan/current/interproscan.sh -i /hps/nobackup/production/interpro/sw/interproscan/current/test_all_appl.fasta -o interproscan_test_all_appl.tsv -f tsv
+    if(!-e "$cwd/i5_analysis/$taxon.$chunk.tsv" or !-s "$cwd/i5_analysis/$taxon.$chunk.tsv"){
+      if(-e "$cwd/i5_logs/$taxon.$chunk.out"){
+        unlink("$cwd/i5_logs/$taxon.$chunk.out");
+      }
+      system( "bsub -o $cwd/i5_logs/$taxon.$chunk.out -q production-rh7 -n 8 -M 8000 -J i5onhps \"/hps/nobackup/production/interpro/sw/interproscan/current/interproscan.sh -appl tigrfam,pfam,cdd,panther,hamap,prints,pirsf,smart  -i $cwd/$file -o $cwd/i5_analysis/$taxon.$chunk.tsv -f tsv\"" );
+      #warn "Missing $file\n";
+    }
+    $chunk--;
+  }
+}
+
+
+#Need to write wait function here.....
+my $incomplete = 1;
+while($incomplete){
+  my $missing = 0;
+  
+  print "checking that all I5 jobs have finished\n";
+
+  for my $taxon (keys %$taxids){
+    my $chunk = $taxids->{$taxon}->{chunk};
+    while($chunk){
+      my $file = "$taxon.$chunk.tsv";
+      $missing++ if(!-e "$cwd/i5_analysis/$file");
+      $chunk--;
+    }
   }
 
-
+  if($missing == 0){
+    print "All I5 jobs complete\n";
+    $incomplete = 0;
+  }else{
+    print "There are $missing files, will wait 10 minutes and check again.\n";
+    sleep(600);
+  }
 }
+
+
+
+
+for my $taxon (keys %$taxids){
+  print "Joining files for $taxon\n";
+  my $chunk = $taxids->{$taxon}->{chunk};
+
+  my $all_i5;
+  while($chunk){
+    my $file = "$taxon.$chunk.tsv";
+    $all_i5 .= read_file("$cwd/i5_analysis/$file");
+    $chunk--;
+  }
+  write_file("$cwd/i5_analysis/$taxon.tsv", $all_i5);
+  
+  
+}
+
+
+
 
 
 sub help {
