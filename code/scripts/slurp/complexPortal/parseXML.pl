d3ee8743a39c9060c233df3ca97f45c8b52c5793
Rob Finn <robert.d.finn@gmail.com>
2018-06-25 13:59:42 +0100

    Added coded to add in InterPro entries based on type.

--- code/scripts/slurp/complexPortal/parseXML.pl
+++ code/scripts/slurp/complexPortal/parseXML.pl
@@ -36,6 +36,23 @@ if($options{help}){
 }
 
 
+my $iprTSV = "interpro_entry2method_20180620.tsv";
+#ENTRY_AC        ENTRY_TYPE      METHOD_AC
+#IPR000001       D       PF00051
+#IPR000001       D       PS50070
+#IPR000001       D       SM00130
+#IPR000001       D       cd00108
+
+open(I, "<", $iprTSV) or die "Could not open the interpro TSV file, $iprTSV\n"; 
+my $iprs;
+while(<I>){
+  next unless( /^IPR/); 
+  chomp;
+  my($acc, $type, $method) = split(/\s+/, $_);
+  push(@{ $iprs->{$type}->{$acc} }, $method);
+}
+close(I);
+
 my $cpUrl = "ftp://ftp.ebi.ac.uk/pub/databases/intact/complex/current/psi30/ecoli";
 my $content;
 my $response = $ua->get($cpUrl);
@@ -46,11 +63,14 @@ if ( $response->is_success ) {
 }
 
 
+
+
+
 my $outDir = "/tmp/GP";
 foreach my $line (split(/\n/, $content)){
   my @row = split(/\s+/, $line);
   my $file = pop(@row);
-
+  p($file);
   mkdir("/tmp/GP/$file");
   chdir("/tmp/GP/$file") or die "Could not change into /tmp/GP/$file:[$!]\n";
    
@@ -103,16 +123,25 @@ foreach my $e ($ipns->get_nodelist){
                              RM => $pmid, 
                              RT => $refObj->{title}, 
                              RA => $refObj->{authorString}, 
-                             RL => $refObj->{journalTitle}.". ".$refObj->{pubYear}.";".$refObj->{journalVolume}.":".$refObj->{pageInfo}
-  });
+                             RL => $refObj->{journalTitle}.". ".$refObj->{pubYear}.";".$refObj->{journalVolume}.":".
+                             (defined($refObj->{pageInfo})? $refObj->{pageInfo} : "-") });
     $rn++;
   }
 }
 
-#Capture the Complex protal accession, which is names as id in the XML.
+#Capture the Complex protal accession, which is named as id in the XML.
+my $cpid;
 my $icpns = $xp->find("/entrySet/entry/interactionList/abstractInteraction/xref/secondaryRef[\@db='complex portal']");
 foreach my $e ($icpns->get_nodelist){
   push(@{$data->{DBREFS}}, {db_id => "Complex Portal", db_link=> $e->getAttribute("id")});
+  $cpid = $e->getAttribute("id");
+}
+
+my @go;
+my $gns = $xp->find("/entrySet/entry/interactionList/abstractInteraction/xref/secondaryRef[\@db='go']");
+foreach my $e ($gns->get_nodelist){
+  next if($e->getAttribute("refType") eq "component"); 
+  push(@go, $e->getAttribute("id")."-".$e->getAttribute("refType"));
 }
 
 #Now add wwpdb cross references
@@ -173,8 +202,18 @@ foreach my $node ($ns->get_nodelist) {
 
   $step->{RQ} = 1;
   my $second = $xp->find("./xref/secondaryRef[\@db='interpro']", $node);
-  foreach my $p ($second->get_nodelist){
-    push(@{$step->{EVID}}, { ipr => $p->getAttribute("id"), sig => "xxx", sc => "sufficient"}); 
+  foreach my $t (qw(F D R)){
+    my $found =0;
+    foreach my $p ($second->get_nodelist){
+      my $thisIpr = $p->getAttribute("id");
+      if($iprs->{$t}->{$thisIpr}){
+        $found++;
+        foreach my $sig (@{ $iprs->{$t}->{$thisIpr} }){
+          push(@{$step->{EVID}}, { ipr => $thisIpr, sig => $sig, sc => "sufficient"}); 
+        }
+      }
+    }
+    last if $found;
   }
   push(@{$data->{STEPS}}, $step);
 }
@@ -187,10 +226,23 @@ $gp->fromDESC($data);
 $gp->toDESC;
 
 #Now print the fasta file. Note this requires some attention when looping.
-open(FA, ">", "fasta") or die "Could not open fasta:[$!]\n";
+open(FA, ">", "FASTA") or die "Could not open FASTA:[$!]\n";
 foreach my $f (@fasta){
   print FA $f;
 }
 close(FA);
-exit;
+
+
+open(S, '>', "status") or die "Could not open status file:[$!]\n";
+print S "checked:\t0\npublic:\t0\n--\n";
+close(S);
+
+
+open(G, '>', "GOterms") or die "Can not open GOterms file\n";
+print G "$cpid\n";
+foreach my $g (@go){
+  print G "$g\n";
+}
+close(G);
+
 }
